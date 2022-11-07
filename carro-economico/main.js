11ebcdedb46deb45070fb78d90115ef970244349
Rizzardd <87292423+Rizzardd@users.noreply.github.com>
2022-11-07 06:56:37 -0400

    Um novo modelo de carro, super econômico foi lançado.

---
+++ carro-economico/main.js
@@ -0,0 +1,4 @@
+function calc() {
+    let money = prompt("informe sua quantia monetária: ")
+    alert("Você pode comprar: "+(money/5)+" Litros de combustivel e conseguem rodar: "+ (money/5*(30))+" Km")
+}
