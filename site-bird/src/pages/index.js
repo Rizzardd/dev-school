a28340b8bc57f114ddc1f365894d6b2e2e57bc75
Monique de Jesus Silva <87292423+Rizzardd@users.noreply.github.com>
2023-02-15 21:19:20 -0400

    criação da estrutura do site

---
+++ site-bird/src/pages/index.js
@@ -0,0 +1,12 @@
+import Header from "../components/common/Header";
+
+
+export default function Home() {
+  return (
+    <>
+     <main className="flex flex-col">
+      <Header/>
+     </main>
+    </>
+  )
+}
